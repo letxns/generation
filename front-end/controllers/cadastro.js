c84d13d2419c3787aa9061c63bec8281764d6ee2
letxns <lele_martins1999@yahoo.com.br>
2022-07-11 18:26:27 -0300

    cadastro front

--- front-end/controllers/cadastro.js
+++ front-end/controllers/cadastro.js
@@ -1,23 +1,17 @@
-// $("#button-send").click(function(e){
-//     e.preventDefault();
-//     const date = $("#input-date").val();
-//     const key = "twas7PMQufH9W9FyyNvT7bpGCEQ29cx4gt2XNoTZ";
-//     $.ajax({
-//         url: `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${date}`,
-//         type: "GET",
-//         success: function(result){
-//             const media = $(".media");    
-//             console.log(result);
-//             $(".title").text(result.title);
-//             $(".author").html(`<strong>Author:</strong> ${result.copyright}`);
-//             $(".media").attr("src", result.url);  
-//             $(".text").text(result.explanation);
+$("#btnInscrever").click(function(e){
+    e.preventDefault();
+    $.ajax({
+        url: `localhost:3000/cadastro`,
+        type: "GET",
+        success: function(result){
+            console.log(result);
+            $(".nome").text(result.nome);
+            $(".dataNascimento");
+            $(".periodo").text(result.periodo);  
+            $(".tipoCurso").text(result.tipoCurso);
 
-//             if(result.media_type == "image"){
-//                 media.html(`<img class="media" src="${result.url}"/>`)
-//             }else{
-//                 media.html(`<iframe class="media" src="${result.url}"> </iframe>`)
-//             }
-//         },
-//     });
-// });
+        },
+    });
+});
+
+// Não consegui tempo para finalizar
