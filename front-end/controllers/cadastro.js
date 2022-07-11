4c1e211606b61844e7ef91ef069b763f3fcbe1c3
letxns <lele_martins1999@yahoo.com.br>
2022-07-11 16:39:16 -0300

    front da aplicação

---
+++ front-end/controllers/cadastro.js
@@ -0,0 +1,23 @@
+// $("#button-send").click(function(e){
+//     e.preventDefault();
+//     const date = $("#input-date").val();
+//     const key = "twas7PMQufH9W9FyyNvT7bpGCEQ29cx4gt2XNoTZ";
+//     $.ajax({
+//         url: `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${date}`,
+//         type: "GET",
+//         success: function(result){
+//             const media = $(".media");    
+//             console.log(result);
+//             $(".title").text(result.title);
+//             $(".author").html(`<strong>Author:</strong> ${result.copyright}`);
+//             $(".media").attr("src", result.url);  
+//             $(".text").text(result.explanation);
+
+//             if(result.media_type == "image"){
+//                 media.html(`<img class="media" src="${result.url}"/>`)
+//             }else{
+//                 media.html(`<iframe class="media" src="${result.url}"> </iframe>`)
+//             }
+//         },
+//     });
+// });
